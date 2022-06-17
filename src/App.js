e090ee3004e0352677ca6c9b201775bf3565a718
Tawfiek <>
2022-06-17 21:18:15 +0200

    add nice message

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        Application has been updated !
+        Application has been update, Good luck with your next step, it was nice to teaching you all!
       </div>
     )
   }
